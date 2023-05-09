5a9884bce78dbad70f009e4bd737d698b82c7acc
FacundoEsquivel97 <esquivelfacundo.97@gmail.com>
2023-05-09 17:15:30 -0300

    Agregado boton de favoritos!
    Se manipula el local storage para guardar el id de los favoritos y a partir de este randerizar la lista de favoritos!

--- script.js
+++ script.js
@@ -1,8 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector('.container');
+  const containerFavoritos = document.querySelector('.favoritos');
   const tmp = document.getElementById('tmp').content;
   const buscadorButton = document.getElementById('buscadorB');
   const randomButton = document.getElementById('randomB');
+
+  const renderFavoritos = () => {
+    let favoritos = localStorage.getItem("favoritos") || "[]";
+    favoritos = JSON.parse(favoritos);
+
+    for (var x = 0; x < favoritos.length; x++) {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${favoritos[x]}/`)
+      .then (response => response.json())
+      .then (pokemon => {
+        const clone = tmp.cloneNode(true);
+        const fragment = document.createDocumentFragment();
+      
+        clone.querySelector('.front_sprite').setAttribute('src',pokemon.sprites.front_default);
+        clone.querySelector('.name').textContent = pokemon.name;
+        pokemon.types.forEach(e => {
+          const typeText = document.createElement('div');
+          typeText.className = 'type_text';
+          typeText.textContent = e.type.name;
+          clone.querySelector('.type').appendChild(typeText)})
+        clone.querySelector('.stats .hp').innerHTML = `HP ${pokemon.stats[0].base_stat}`
+        clone.querySelector('.stats .attack').innerHTML = `ATTACK ${pokemon.stats[1].base_stat}`
+        clone.querySelector('.stats .defense').innerHTML = `DEFENSE ${pokemon.stats[2].base_stat}`
+        clone.querySelector('.stats .special_attack').innerHTML = `SPECIAL ATTACK ${pokemon.stats[3].base_stat}`
+        clone.querySelector('.stats .special_defense').innerHTML = `SPECIAL DEFENSE ${pokemon.stats[4].base_stat}`
+        clone.querySelector('.stats .speed').innerHTML = `SPEED ${pokemon.stats[5].base_stat}`
+    
+        const favButton = clone.getElementById('buttonFav');
+        favButton.addEventListener('click',() => {
+          favear(pokemon)
+        });
+      
+        fragment.appendChild(clone);
+        containerFavoritos.appendChild(fragment);
+      })
+      .catch (error => console.log(error));
+    }
+    
+  }
+
+  const favear = (pokemon) => {
+    let pokeId = pokemon.id;
+    let favoritos = localStorage.getItem("favoritos") || "[]";
+    favoritos = JSON.parse(favoritos);
+
+    if (favoritos.includes(pokeId)) {
+      let posId = favoritos.findIndex(e => e == pokeId);
+      favoritos.splice(posId,1);
+    } else {
+      favoritos.push(pokeId);
+    }
+    localStorage.setItem("favoritos", JSON.stringify(favoritos));
+    console.log(favoritos)
+    while (containerFavoritos.firstChild) {
+      containerFavoritos.removeChild(containerFavoritos.lastChild);
+    }
+    renderFavoritos();
+  }
 
 const renderPokemon = id => {
   fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
@@ -24,6 +82,11 @@ const renderPokemon = id => {
     clone.querySelector('.stats .special_attack').innerHTML = `SPECIAL ATTACK ${pokemon.stats[3].base_stat}`
     clone.querySelector('.stats .special_defense').innerHTML = `SPECIAL DEFENSE ${pokemon.stats[4].base_stat}`
     clone.querySelector('.stats .speed').innerHTML = `SPEED ${pokemon.stats[5].base_stat}`
+
+    const favButton = clone.getElementById('buttonFav');
+    favButton.addEventListener('click',() => {
+      favear(pokemon)
+    });
   
     fragment.appendChild(clone);
     while (container.firstChild) {
@@ -42,6 +105,7 @@ const renderPokemon = id => {
     renderPokemon(poke.toLowerCase())
     document.getElementById('buscador').value = ''
   })
-  randomButton.addEventListener('click',() => {randomPoke()})
+  randomButton.addEventListener('click',() => {randomPoke()});
   randomPoke()
+  renderFavoritos()
 })
